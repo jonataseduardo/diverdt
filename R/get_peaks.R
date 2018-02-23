b67852704eedad5f90a446ba88dfa162534948fc
jonataseduardo <jonataseduardo@gmail.com>
2018-02-23 18:31:35 -0300

    using shift

--- R/get_peaks.R
+++ R/get_peaks.R
@@ -14,7 +14,7 @@
 #' @return data.table 
 
 get_peaks <-
-  function(data_raw, data_sum, col_name, score_th, window_s, greater = TRUE){
+  function(data_raw, data_sum, col_name, score_th, step_s, greater = TRUE){
 
     score_th <- 0.01
     col_name <- 'p.value'
@@ -27,22 +27,9 @@ get_peaks <-
     setkey(data_sum, CHR, POS, CM, SNP)
 
     DT <- data_sum[score_col < score_th][data_raw]
-    list_idx <- DT[!is.na(score_col)][, idx]
-
-    n <- length(list_idx)
-
-    i <- 1
-    
-    id_aux <- list()
-    
-    for(i in 1:n){
-      id_aux[[i]] <- list_idx[(1 + i):n] - list_idx[1:(n - i)] <= window_s
-      if (!any(id_aux[[i]])) break
-    }
-    id_aux
-    i
-
-    list_idx[2:(n - 1)][id_aux]
+    gg<-  DT[!is.na(score_col)]
+    gg[, idx_next := idx - shift(idx)]
+    gg
 
   }
 
